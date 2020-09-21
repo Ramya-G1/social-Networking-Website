d8fa3895dae3db84b9413ce06edad250f7fd8d04
Ramya <ramyagatti361@gmail.com>
2020-09-21 12:34:33 +0530

    procfile

--- server/models/user.js
+++ server/models/user.js
@@ -18,7 +18,7 @@ const userSchema=new mongoose.Schema({
     expireToken:Date,
     pic:{
         type:String,
-        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+default:"https://res.cloudinary.com/dwa4ixyyh/image/upload/v1599733784/sample.jpg"
     },
     followers:[
         {type:ObjectId,ref:"User"}
